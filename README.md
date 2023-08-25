96ec3df5c5b7977554dcc8e8ebb09ebe2aafbb26
Kirito <efraimcanilang@gmail.com>
2023-08-25 14:35:41 +0800

    Added implementation of onefile build

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WebSocketMessageSender
 For Testing WebSocket - Personal Use Only
+
+# Commands to Build
+pyinstaller --add-data “res/*.json:res” -F main.py
+pyinstaller --onefile --add-data “res/*.json:res” -F main.py
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import json
+import os
+import sys
+
 import websocket
 import time
+
+
+def get_resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 
 def on_message(ws, message):
@@ -26,13 +40,13 @@ def send_messages():
         if start == "y":
             time.sleep(5)
             # Join Room
-            with open('JoinRoom.json', 'r') as json_file:
+            with open(get_resource_path('res/JoinRoom.json'), 'r') as json_file:
                 data = json.load(json_file)
                 ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
             # Start Betting
             time.sleep(5)
-            with open('StartBet.json', 'r') as json_file:
+            with open(get_resource_path('res/StartBet.json'), 'r') as json_file:
                 data = json.load(json_file)
                 ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
@@ -40,72 +54,71 @@ def send_messages():
 
             # Card Distribution
             time.sleep(3)
-            with open('CardDistribution.json', 'r') as json_file:
+            with open(get_resource_path('res/CardDistribution.json'), 'r') as json_file:
                 data = json.load(json_file)
                 ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
 
 def player_betting():
-
     # Player 1 Finished Bet
     time.sleep(2)
-    with open('Player1Bet.json', 'r') as json_file:
+    with open(get_resource_path('res/Player1Bet.json'), 'r') as json_file:
         data = json.load(json_file)
         ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
     time.sleep(2)
-    with open('Player1BetFinished.json', 'r') as json_file:
+    with open(get_resource_path('res/Player1BetFinished.json'), 'r') as json_file:
         data = json.load(json_file)
         ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
     # Player 2 Finished Bet
     time.sleep(2)
-    with open('Player2Bet.json', 'r') as json_file:
+    with open(get_resource_path('res/Player2Bet.json'), 'r') as json_file:
         data = json.load(json_file)
         ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
     time.sleep(2)
-    with open('Player2BetFinished.json', 'r') as json_file:
+    with open(get_resource_path('res/Player2BetFinished.json'), 'r') as json_file:
         data = json.load(json_file)
         ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
     # Player 3 Finished Bet
     time.sleep(2)
-    with open('Player3Bet.json', 'r') as json_file:
+    with open(get_resource_path('res/Player3Bet.json'), 'r') as json_file:
         data = json.load(json_file)
         ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
     time.sleep(2)
-    with open('Player3BetFinished.json', 'r') as json_file:
+    with open(get_resource_path('res/Player3BetFinished.json'), 'r') as json_file:
         data = json.load(json_file)
         ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
     # Player 4 Finished Bet
     time.sleep(2)
-    with open('Player4Bet.json', 'r') as json_file:
+    with open(get_resource_path('res/Player4Bet.json'), 'r') as json_file:
         data = json.load(json_file)
         ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
     time.sleep(2)
-    with open('Player4BetFinished.json', 'r') as json_file:
+    with open(get_resource_path('res/Player4BetFinished.json'), 'r') as json_file:
         data = json.load(json_file)
         ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
     # Player 5 Finished Bet
     time.sleep(2)
-    with open('Player5Bet.json', 'r') as json_file:
+    with open(get_resource_path('res/Player5Bet.json'), 'r') as json_file:
         data = json.load(json_file)
         ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
     time.sleep(2)
-    with open('Player5BetFinished.json', 'r') as json_file:
+    with open(get_resource_path('res/Player5BetFinished.json'), 'r') as json_file:
         data = json.load(json_file)
         ws.send(json.dumps(data), opcode=websocket.ABNF.OPCODE_TEXT)
 
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://socketsbay.com/wss/v2/9/05db90c5178c173e63af62155ce998e8/",
+    ws = websocket.WebSocketApp("wss://socketsbay.com/wss/v2/4/05db90c5178c173e63af62155ce998e8/",
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
